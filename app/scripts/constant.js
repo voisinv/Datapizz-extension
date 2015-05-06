9a1f6ec6cb3a4fd57e1c48fffd891ddaec242195
voisinv <vincent.voisin@hotmail.fr>
2015-05-06 17:27:55 +0200

    pizza server

--- app/scripts/constant.js
+++ app/scripts/constant.js
@@ -1,14 +1,14 @@
 /**
  * Created by Mac-Vincent on 21/04/15.
  */
-var URL_SERVER = 'http://localhost';
-var PORT_URL = '3000';
+var URL_SERVER = 'https://datapizz.herokuapp.com';
+var PORT_URL = '';
 var COMPLETE_URL = URL_SERVER + ':' + PORT_URL;
 
 
 var urlToCall = {
-  getAll : COMPLETE_URL + '/tags',
-  setArticle : COMPLETE_URL + '/articles'
+  getAll : COMPLETE_URL + '/api/tags',
+  setArticle : COMPLETE_URL + '/api/articles'
 }
 
 angular.module('constant', []).constant('URLS', urlToCall);
