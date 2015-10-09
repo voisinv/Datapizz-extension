bf86eb5e81b198969e19b255b96918d2bd9512d0
voisinv <vincent.voisin@hotmail.fr>
2015-10-10 00:04:14 +0200

    update tags to the server

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -2,22 +2,29 @@
 
 var extensionCtrl = function($scope, $firebaseObject) {
   var self = this;
-  var ref = new Firebase("https://datapizzz.firebaseio.com/");
+  var ref = new Firebase("https://test-datapizz.firebaseio.com/");
 
   console.log('firebase obj', $firebaseObject(ref))
   self.data = $firebaseObject(ref);
 
   self.data.$loaded()
     .then(function() {
-      self.tags = self.data.tags.map(function(e) {
-        return {value: e.value, selected: false};
-      });
+      self.keys = self.data.tags ? _.keys(self.data.tags) : [];
+      self.tags = self.data.tags ? _.values(self.data.tags).map(function(e, i) {
+        e.key = self.keys[i];
+        return e;
+      }) : [];
+
     });
 
   self.addTag = function() {
+
+    var index = _.indexOf(_.map(self.tags, function(e) {return e.value}), self.nameNewTag)
+    if (index >= 0) {self.tags[index].selected = true; return;}
     self.newTag  = {value: self.nameNewTag};
     self.newTag.selected = true;
     self.tags.push(self.newTag);
+
     self.nameNewTag = '';
   };
 
@@ -28,29 +35,41 @@ var extensionCtrl = function($scope, $firebaseObject) {
   self.save = function() {
     var tags = self.tags.filter(function(e) {
       return e.selected;
-    }).map(function(e) {
-      return e.value;
+    });
+
+    var refTags = ref.child('tags');
+    tags.forEach(function(tag) {
+      if (tag.hasOwnProperty('key')) {
+        refTags
+          .child(tag.key)
+          .update({radius: tag.radius + 5})
+      } else {
+        refTags
+          .push({
+            radius: 5,
+            value: tag.value
+          });
+      }
     });
 
     ref.child('articles')
       .push({
-        id: 10,
         tags: tags,
         url: self.url,
         title: self.title
-      })
+      });
+
   };
 
   self.goApp = function() {
     chrome.tabs.create({url: 'http://pizzaaa.herokuapp.com'})
-  }
+  };
 
   chrome.tabs.getSelected(null,function(onglet){
     console.log('onglet', onglet);
     var url = onglet.url;
     self.url = url.substr(url.indexOf('://')+3)
     self.title=onglet.title;
-
   });
 };
 
