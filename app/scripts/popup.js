71e69a24f507eddf91077b9aa2edb90b297d3ac2
matt2mi <2m1tema@gmail.com>
2015-10-11 13:18:01 +0200

    loader + maj bower

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,73 +1,73 @@
 'use strict';
 
-var extensionCtrl = function($scope, $firebaseObject) {
-  var self = this;
-  var ref = new Firebase("https://test-datapizz.firebaseio.com/");
+var extensionCtrl = function($scope, $firebaseObject, $interval) {
+    var self = this;
+    var ref = new Firebase("https://test-datapizz.firebaseio.com/");
 
-  console.log('firebase obj', $firebaseObject(ref))
-  self.data = $firebaseObject(ref);
-  self.tags = [];
+    console.log('firebase obj', $firebaseObject(ref));
+    self.data = $firebaseObject(ref);
+    self.tags = [];
+    self.loading = true;
 
-  self.newVeg = function(chip) {
-    return {
-      name: chip,
-      type: 'unknown'
+    self.newVeg = function(chip) {
+        return {
+            name: chip,
+            type: 'unknown'
+        };
     };
-  };
 
-  self.data.$loaded()
-    .then(function() {
-      self.keys = self.data.tags ? _.keys(self.data.tags) : [];
-      self.existingTags = self.data.tags ? _.values(self.data.tags).map(function(e, i) {
-        e.key = self.keys[i];
-        return e;
-      }) : [];
-
+    self.data.$loaded().then(function() {
+        self.keys = self.data.tags ? _.keys(self.data.tags) : [];
+        self.existingTags = self.data.tags ? _.values(self.data.tags).map(function(e, i) {
+            e.key = self.keys[i];
+            self.loading = false;
+            return e;
+        }) : [];
     });
 
-  self.save = function() {
+    self.save = function() {
 
-    var refTags = ref.child('tags');
-    self.tags.forEach(function (tag) {
-      var index = _.indexOf(_.map(self.existingTags, function(e) {return e.value}), tag);
-      if (index >= 0) {
-        refTags
-          .child(self.keys[index])
-          .update({radius: self.existingTags[index].radius + 5})
-      } else {
-        refTags
-          .push({
-            radius: 5,
-            value: tag
-          });
-      }
+        var refTags = ref.child('tags');
+        self.tags.forEach(function (tag) {
+            var index = _.indexOf(_.map(self.existingTags, function(e) {return e.value}), tag);
+            if (index >= 0) {
+                refTags
+                    .child(self.keys[index])
+                    .update({radius: self.existingTags[index].radius + 5})
+            } else {
+                refTags
+                    .push({
+                        radius: 5,
+                        value: tag
+                    });
+            }
+        });
+
+        ref.child('articles')
+            .push({
+                tags: self.tags,
+                url: self.url,
+                title: self.title
+            });
+    };
+
+    self.goApp = function() {
+        chrome.tabs.create({url: 'http://pizzaaa.herokuapp.com'})
+    };
+
+    chrome.tabs.getSelected(null,function(onglet){
+        var url = onglet.url;
+        self.url = url.substr(url.indexOf('://')+3)
+        self.title=onglet.title;
     });
-
-    ref.child('articles')
-      .push({
-        tags: self.tags,
-        url: self.url,
-        title: self.title
-      });
-  };
-
-  self.goApp = function() {
-    chrome.tabs.create({url: 'http://pizzaaa.herokuapp.com'})
-  };
-
-  chrome.tabs.getSelected(null,function(onglet){
-    var url = onglet.url;
-    self.url = url.substr(url.indexOf('://')+3)
-    self.title=onglet.title;
-  });
 };
 
 angular.module('datapizz-extension', ['ngMaterial', 'firebase', 'constant'])
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
-      .backgroundPalette('grey')
-      .warnPalette('red')
-      .accentPalette('pink');
-  })
-.controller('ExtensionCtrl', extensionCtrl);
+    .config(function($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .backgroundPalette('grey')
+            .warnPalette('red')
+            .accentPalette('pink');
+    })
+    .controller('ExtensionCtrl', extensionCtrl);
